03ab8b42fec7c6d9690a7141538094b09253b256
Nikita Savchenko <Nikita Savchenko>
2016-10-25 17:01:42 +0300

    index

--- app/js/script.js
+++ app/js/script.js
@@ -1,3 +1,14 @@
 $(document).ready(function () {
 
+    // слайдер на главной странице(смена фона)
+    var owl = $('.owl-carousel');
+    owl.owlCarousel({
+        items:1,
+        loop:true,
+        animateOut: 'fadeOut',
+        autoplay:true,
+        autoplayTimeout:10000,
+        autoplayHoverPause:true
+    });
+
 });
