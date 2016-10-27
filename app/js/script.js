70faa27d2bb9be113fca9f7e949dba863a6dff7b
Nikita Savchenko <Nikita Savchenko>
2016-10-27 16:53:20 +0300

    index

--- app/js/script.js
+++ app/js/script.js
@@ -11,4 +11,23 @@ $(document).ready(function () {
         autoplayHoverPause:true
     });
 
+    /*go up arrow*/
+    $(window).scroll(function () {
+        var scrollDiv = $('#Go_Top');
+        $(this).hide().removeAttr("href");
+        if ($(window).scrollTop() >= "250") {/*показывать при прокрутке вниз показывать медленно #Go_Top*/
+            $(this).fadeIn("slow")
+        };
+        if ($(window).scrollTop() <= "250") {/*при прокрутке ввверх медленно скрывать #Go_Top*/
+            $(scrollDiv).fadeOut("slow");
+        }
+        else {
+            $(scrollDiv).fadeIn("slow")
+        };
+    });
+    $('#Go_Top').click(function () {/*функция плавной прокрутки вверх при клике на стрелку "вверх"*/
+        $("html, body").animate({scrollTop: 0}, "slow");
+        return false;
+    });
+    /*close go up arrow*/
 });
