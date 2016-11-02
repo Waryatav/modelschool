bd709d5ddec0c3492c040ab917eee3032537e690
AKD47 <van83horn@gmail.com>
2016-11-02 14:03:27 +0300

    new structure

--- gulpfile.js
+++ gulpfile.js
@@ -27,23 +27,29 @@ var postcss = require('gulp-postcss'),//Блиотека-парсер стиле
     fontmagic = require('postcss-font-magician'),
     fixes = require('postcss-fixes');
 
-
-
 gulp.task('css-libs', function() { // Создаем таск css-libs
     var processors = [
         cssnano
     ]
     return gulp.src([
-            'app/libs/normalize-css/normalize.css'
-        ]) // Берем источник
+        'app/libs/**/*.css'
+    ]) // Берем источник
         .pipe(postcss(processors))// сжымаем
         .pipe(concat('libs.min.css'))// объеденяем в файл
         .pipe(gulp.dest('css')) // Выгружаем результата в папку app/css
-        // .pipe(browserSync.reload({
-        //     stream: true
-        // })
-        //) // Обновляем CSS на странице при изменении
+        .pipe(browserSync.reload({
+            stream: true
+        })) // Обновляем CSS на странице при изменении
 });
+
+/*gulp.task('js-libs', function() {
+    return gulp.src([ // Берем все необходимые библиотеки
+            'app/libs/jquery/dist/jquery.min.js'
+        ])
+        .pipe(concat('libs.min.js')) // Собираем их в кучу в новом файле libs.min.js
+        .pipe(uglify()) // Сжимаем JS файл
+        .pipe(gulp.dest('js')); // Выгружаем в папку app/js
+});*/
 
 gulp.task('sass', function() { // Создаем таск Sass
     var processors = [
@@ -54,10 +60,10 @@ gulp.task('sass', function() { // Создаем таск Sass
         autoprefixer(['last 5 versions', '> 5%', 'ie 8', 'ie 7'], {
             cascade: true
         }),
-        // pxtorem({
-        //     rootValue: 14,
-        //     replace: false
-        // }),
+        /* pxtorem({
+         rootValue: 14,
+         replace: false
+         }),*/
         focus,
         sorting(),
         stylefmt,
@@ -65,7 +71,7 @@ gulp.task('sass', function() { // Создаем таск Sass
     ];
     return gulp.src('app/sass/**/*.scss')
         .pipe(plumber())
-        .pipe(sourcemaps.init())        
+        .pipe(sourcemaps.init())
         .pipe(sass().on('error', sass.logError))
         .pipe(postcss(processors))
         .pipe(rename({
@@ -75,24 +81,24 @@ gulp.task('sass', function() { // Создаем таск Sass
         .pipe(sourcemaps.write('.', { sourceRoot: 'css-source' }))
         .pipe(plumber.stop())
         .pipe(gulp.dest('css'))
-        // .pipe(browserSync.reload({
-        //     stream: true
-        // }));
+        .pipe(browserSync.reload({
+            stream: true
+        }));
 });
 
-// gulp.task('browser-sync', function() { // Создаем таск browser-sync
-//     browserSync({ // Выполняем browserSync
-//         proxy: {
-//             target: 'loaf_furniture' // Директория для сервера - app
-//         },
-//         ghostMode: {
-//             clicks: true,
-//             forms: true,
-//             scroll: true
-//         },
-//         notify: false // Отключаем уведомления
-//     });
-// });
+gulp.task('browser-sync', function() { // Создаем таск browser-sync
+    browserSync({ // Выполняем browserSync
+        proxy: {
+            target: '' // Директория для сервера - app
+        },
+        ghostMode: {
+            clicks: true,
+            forms: true,
+            scroll: true
+        },
+        notify: false // Отключаем уведомления
+    });
+});
 
 gulp.task('compress', ['clean'], function() {// Создаем таск compress
     return gulp.src('app/js/*.js')// Берем все необходимые библиотеки
@@ -110,27 +116,30 @@ gulp.task('compress', ['clean'], function() {// Создаем таск compress
 });
 
 gulp.task("clean", function (cb) {
-  rimraf('./js/script.min.js', cb);
+    rimraf('./js/script.min.js', cb);
 });
 
-gulp.task('extend', function () {
+gulp.task('extend-pages', function () {
     gulp.src('./app/html/pages/*.html')
         .pipe(extender({annotations:true,verbose:false})) // default options
         .pipe(gulp.dest('./'))
-
 });
 
-gulp.task('watch', ['compress', 'extend', 'css-libs', 'img', 'sass'], function() {
+gulp.task('extend-blocks', function () {
+    gulp.src('./app/html/*.html')
+        .pipe(extender({annotations:true,verbose:false})) // default options
+        .pipe(gulp.dest('./'))
+});
+
+gulp.task('watch', ['compress', 'extend-pages', 'css-libs', 'img', 'sass'], function() {
     gulp.watch('app/libs/**/*', ['css-libs']); // Наблюдение за папкой libs
     gulp.watch('app/img/**/*', ['img']);// Наблюдение за папкой img
     gulp.watch('app/sass/**/*.scss', ['sass']); // Наблюдение за sass файлами в папке sass
-    gulp.watch(['app/html/pages/*.html'], ['extend']);// Наблюдение за HTML-файлами
-    gulp.watch(['app/html/*.html'], ['extend']);// Наблюдение за HTML-файлами
-    //gulp.watch('./**/*.html', browserSync.reload); // Наблюдение за HTML-файлами
-    // gulp.watch('app/js/*', function() {
-    //     gulp.run('compress');
-    // }, browserSync.reload); // Наблюдение за JS файлами в папке js
+    gulp.watch(['app/html/**/*.html'], ['extend-pages']);// Наблюдение за HTML-файлами в папке html/pages
+   /* gulp.watch(['app/html/!*.html'], ['extend-blocks']);// Наблюдение за HTML-файлами в папке html*/
+    gulp.watch('app/js/**/*.js', ['compress']); // Наблюдение за js-файлами
 });
+
 
 gulp.task('img', function() {
     return gulp.src('app/img/**/*')
@@ -143,18 +152,18 @@ gulp.task('img', function() {
             use: [pngquant()]
         })))
         .pipe(gulp.dest('img'))
-        // .pipe(browserSync.reload({
-        //     stream: true
-        // }));
+        .pipe(browserSync.reload({
+            stream: true
+        }));
 });
 
-/*
-gulp.task('build', ['img', 'sass', 'scripts'], function() {
+
+/*gulp.task('build', ['img', 'sass', 'scripts'], function() {
 
     var buildCss = gulp.src([ // Переносим библиотеки в продакшен
-        'app/css/main.css',
-        'app/css/libs.min.css'
-    ])
+            'app/css/main.css',
+            'app/css/libs.min.css'
+        ])
         .pipe(gulp.dest('css'))
 
     var buildFonts = gulp.src('app/fonts/!**!/!*') // Переносим шрифты в продакшен
@@ -165,9 +174,11 @@ gulp.task('build', ['img', 'sass', 'scripts'], function() {
 
 });*/
 
-
 gulp.task('clear', function(callback) {
     return cache.clearAll();
 });
 
 gulp.task('default', ['watch']);
+
+/*
+ npm i gulp gulp-sass browser-sync gulp-concat gulp-uglifyjs gulp-rename del gulp-imagemin imagemin-pngquant gulp-cache gulp-html-extend gulp-sourcemaps rimraf yargs gulp-plumber gulp-postcss autoprefixer cssnano postcss-pxtorem postcss-short stylefmt postcss-assets postcss-short-spacing postcss-focus postcss-sorting postcss-font-magician postcss-fixes --save-dev*/
